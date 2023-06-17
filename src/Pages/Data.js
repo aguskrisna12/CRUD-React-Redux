23ea1571b93ad6bece255a0a11a8430eea132248
aguskrisna12 <aguskrisnau317@gmail.com>
2023-06-17 11:43:56 +0800

    add new pages

---
+++ src/Pages/Data.js
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name : 'satu',
+        email: 'satu@gmail.com',
+        id: 1
+    },
+    {
+        name : 'dua',
+        email: "dua@gmail.com",
+        id: 2
+    },
+    {
+        name : 'tiga',
+        email : 'tiga@gmail.com',
+        id: 3
+    }
+]
